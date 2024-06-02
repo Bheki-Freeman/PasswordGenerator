515eb26fba6a8e6bbb65d632633ab900e8eafd1a
Bheki-Freeman <bhekisisadlamini05@gmail.com>
2024-06-02 20:47:44 +0200

    Create the generate method

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 import random
 import string # I will be working with characters to strings
 
+def create_password(length:int) -> None: #returns void
+    if length < 4:
+        raise ValueError('Password is too short!')
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = []
+    password.append(random.choice(string.ascii_lowercase)) # Start to add
+    password.append(random.choice(string.ascii_uppercase))
+    password.append(random.choice(string.digits))
+    password.append(random.choice(string.punctuation))
+
